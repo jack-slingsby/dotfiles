a8445b7a74a86d93b45c924efcf47fc4fdb506f1
= <jack@theslingsbys.com>
2024-12-18 21:22:12 +0000

    Adds more toggleterm mappings

--- lua/jack/remap.lua
+++ lua/jack/remap.lua
@@ -37,3 +37,17 @@ require('lualine').setup()
 
 -- ********** toggleterm ********** 
 vim.api.nvim_set_keymap('n', '<leader>tt',     ":ToggleTerm<cr>", {silent = true, noremap = true}) 
+
+vim.api.nvim_set_keymap('n', '<leader>tf',     ":ToggleTerm direction=float<cr>", {silent = true, noremap = true}) 
+
+function _G.set_terminal_keymaps()
+  local opts = {buffer = 0}
+  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+--  vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+--  vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+end
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
