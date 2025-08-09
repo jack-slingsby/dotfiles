c75dd94dfde312867b78c7b5fc1229e740e224f5
Jack Slingsby <jack.slingsby@imgtec.com>
2025-08-09 15:39:47 +0100

    Tidy lsp keybindings

--- nvim/lua/jack/lsp.lua
+++ nvim/lua/jack/lsp.lua
@@ -9,7 +9,7 @@ lspconfig_defaults.capabilities = vim.tbl_deep_extend(
 )
 
 require('mason-lspconfig').setup({
-	ensure_installed = { 'lua_ls', 'rust_analyzer' },
+	ensure_installed = { 'lua_ls', 'rust_analyzer', 'clangd' },
 	handlers = {
 		function(server_name)
 			require('lspconfig')[server_name].setup({})
@@ -22,16 +22,16 @@ vim.api.nvim_create_autocmd('LspAttach', {
 	callback = function(event)
 		local opts = { buffer = event.buf }
 
-		vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-		vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-		vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-		vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-		vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-		vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-		vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-		vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-		vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-		vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+		vim.keymap.set('n', '<Leader>gh', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+		vim.keymap.set('n', '<Leader>gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+		vim.keymap.set('n', '<Leader>gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+		vim.keymap.set('n', '<Leader>gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+		vim.keymap.set('n', '<Leader>gt', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+		vim.keymap.set('n', '<Leader>gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+		vim.keymap.set('n', '<Leader>gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+		vim.keymap.set('n', '<Leader>r', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+		vim.keymap.set({ 'n', 'x' }, '<leader>cf', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+		vim.keymap.set('n', '<Leader>ca', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
 
 		local client = vim.lsp.get_client_by_id(event.data.client_id)
 		if client:supports_method('textDocument/completion') then
